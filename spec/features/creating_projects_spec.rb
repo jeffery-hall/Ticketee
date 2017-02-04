9b96d20d68184dde6743f75d7f21b73eedf627e5
roguedevice <hall.jeffw@gmail.com>
2017-02-04 17:59:17 -0500

    Add title function for project show page.

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -6,10 +6,16 @@ RSpec.feature "Users can create new projects" do
 
     click_link "New Project"
 
-    fill_in "Name", with: "Sublime Text 3"
-    fill_in "Description", with: "A text editor for everyone"
+    fill_in "Name", with: "Emacs 25"
+    fill_in "Description", with: "An extensible text editor for everyone"
     click_button "Create Project"
 
     expect(page).to have_content "Project has been created."
+
+    project = Project.find_by(name: "Emacs 25")
+    expect(page.current_url).to eq project_url(project)
+
+    title = "Emacs 25 - Projects - Ticketee"
+    expect(page).to have_title title
   end
 end
