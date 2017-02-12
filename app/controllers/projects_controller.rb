97898fdb226e690eec8952e059bf22c0516a4286
roguedevice <hall.jeffw@gmail.com>
2017-02-12 11:42:33 -0500

    Projects can now be updated.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,6 +26,20 @@ class ProjectsController < ApplicationController
   def edit
     @project = Project.find(params[:id])
   end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render "edit"
+    end
+  end
+
   
   private
 
--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,15 +1,25 @@
 require "rails_helper"
 
 RSpec.feature  "Users can edit existing projects" do
-  scenario "with valid attributes" do
+  before do
     FactoryGirl.create(:project, name: "Emacs 25")
     visit "/"
     click_link "Emacs 25"
     click_link "Edit Project"
+  end
+  
+  scenario "with valid attributes" do
     fill_in "Name", with: "Emacs 26 beta"
     click_button "Update Project"
 
     expect(page).to have_content "Project has been updated."
     expect(page).to have_content "Emacs 26 beta"
+  end
+
+  scenario "when providing invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content "Project has not been updated."
   end
 end
